4a99a81224e62aac3d80cf6a3009789dac0bb8f5
o0Ezequiel0o <ezematteazzi@gmail.com>
2026-04-22 21:48:50 -0300

    GameObjectEventBus -> GlobalEventBus

--- Assets/Scripts/Core/GameObjectEventBus.cs
+++ Assets/Scripts/Core/GameObjectEventBus.cs
@@ -1,91 +1,48 @@
 using System.Collections.Generic;
-using UnityEngine;
 using System;
 
-public static class GameObjectEventBus
+public static class GlobalEventBus
 {
-    private readonly static Dictionary<Type, Dictionary<GameObject, List<Callback>>> events = new Dictionary<Type, Dictionary<GameObject, List<Callback>>>();
+    private readonly static Dictionary<Type, Delegate> events = new Dictionary<Type, Delegate>();
 
-    public static void Invoke<T>(GameObject ownerRef, T eventData)
+    public static void Invoke(IGlobalEvent eventData)
     {
         Type type = eventData.GetType();
 
-        if (events.TryGetValue(type, out Dictionary<GameObject, List<Callback>> inner))
+        if (events.TryGetValue(type, out Delegate @delegate))
         {
-            if (inner.TryGetValue(ownerRef, out List<Callback> callbacks))
-            {
-                for (int i = 0; i < callbacks.Count; i++)
-                {
-                    callbacks[i].@delegate?.DynamicInvoke(eventData);
-                }
-            }
+            @delegate?.DynamicInvoke(eventData);
         }
     }
 
-    public static void Subscribe<T>(GameObject ownerRef, Action<T> action)
-    {
-        Subscribe(ownerRef, action, 0);
-    }
-
-    public static void Subscribe<T>(GameObject ownerRef, Action<T> action, int order)
+    public static void Subscribe<T>(Action<T> action) where T : IGlobalEvent
     {
         Type type = typeof(T);
 
         if (events.ContainsKey(type))
         {
-            events[type][ownerRef].Add(new Callback(action, order));
+            events[type] = Delegate.Combine(events[type], action);
         }
         else
         {
-            events.Add(type, new Dictionary<GameObject, List<Callback>>());
-            events[type].Add(ownerRef, new List<Callback>());
-            events[type][ownerRef].Add(new Callback(action, order));
-
-            List<Callback> callbacks = events[typeof(T)][ownerRef];
-            callbacks.Sort((callback1, callback2) => callback2.order.CompareTo(callback1.order));
+            events[type] = action;
         }
     }
 
-    public static void Unsubscribe<T>(GameObject ownerRef, Action<T> action)
+    public static void Unsubscribe<T>(Action<T> action) where T : IGlobalEvent
     {
         Type type = typeof(T);
 
-        if (events.ContainsKey(type) && events[type].ContainsKey(ownerRef))
+        if (events.ContainsKey(type))
         {
-            RemoveCallback(action, events[type][ownerRef]);
+            events[type] = Delegate.Remove(events[type], action);
 
-            if (events[type][ownerRef].Count <= 0)
-            {
-                events[type].Remove(ownerRef);
-            }
-
-            if (events[type].Count <= 0)
+            if (events[type] == null)
             {
                 events.Remove(type);
             }
         }
     }
-
-    private static void RemoveCallback<T>(Action<T> action, List<Callback> callbacks)
-    {
-        for (int i = 0; i < callbacks.Count; i++)
-        {
-            if (callbacks[i].@delegate == (MulticastDelegate)action)
-            {
-                callbacks.RemoveAt(i);
-            }
-        }
-    }
-
-    public struct Callback
-    {
-        public Delegate @delegate;
-        public int order;
-
-        public Callback(Delegate @delegate, int order)
-        {
-            this.@delegate = @delegate;
-            this.order = order;
-        }
-    }
 }
+
+public interface IGlobalEvent { }
