0897b72f5cdebb843235c7753540d949339b010b
o0Ezequiel0o <ezematteazzi@gmail.com>
2026-04-24 13:04:38 -0300

    Updated item template

--- Assets/Scripts/Item/Items/_Template/TemplateItemData.cs
+++ Assets/Scripts/Item/Items/_Template/TemplateItemData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Zeke.Items
 {
-    [CreateAssetMenu(fileName = "TemplateItem", menuName = "ScriptableObjects/Items/Items/TemplateItem", order = 1)]
+    [CreateAssetMenu(fileName = "Template", menuName = "ScriptableObjects/Items/Items/Template", order = 1)]
     public class TemplateItemData : ItemData
     {
         //Template
