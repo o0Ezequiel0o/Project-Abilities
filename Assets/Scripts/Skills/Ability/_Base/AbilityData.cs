a05aa8f0d5e9f250758fd7afdf7d40027a7f25e1
o0Ezequiel0o <ezematteazzi@gmail.com>
2026-04-28 10:20:20 -0300

    AbilityData visual fields added

--- Assets/Scripts/Skills/Ability/_Base/AbilityData.cs
+++ Assets/Scripts/Skills/Ability/_Base/AbilityData.cs
@@ -10,6 +10,9 @@ namespace Zeke.Abilities
     {
         [field: Header("Display")]
         [field: SerializeField] public Sprite Icon { get; private set; }
+        [field: SerializeField] public string Name { get; private set; }
+
+        [field: SerializeField, TextArea(3, 3)] public string Description { get; private set; } 
 
         [field: Header("Toggling")]
         [field: SerializeField] public AbilityType AbilityType { get; private set; }
