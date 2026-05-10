3b4bb98857c51d81223a5048d35371edadc8f2a4
o0Ezequiel0o <ezematteazzi@gmail.com>
2026-05-10 15:30:38 -0300

    number formatter updated

--- Assets/Scripts/Core/Strings/NumberFormatter.cs
+++ Assets/Scripts/Core/Strings/NumberFormatter.cs
@@ -1,10 +1,58 @@
+using System.Text;
+
 public static class NumberFormatter
 {
+    private static StringBuilder cappedText = new StringBuilder(16);
+    private static StringBuilder text = new StringBuilder(16);
+
+    private static float Hundred => 100f;
+    private static float Thousand => 1000f;
+    private static float Million => 1000000f;
+    private static float Billion => 1000000000f;
+
+    private static int maxLength = 3;
+
     public static string FormatNumber(float number)
     {
-        if (number >= 999f)
-            return (number / 1000f).ToString() + "k";
+        text.Clear();
 
-        return number.ToString();
+        if (number < Thousand)
+        {
+            text.Append(number);
+            return CapLength(text).ToString();
+        }
+        else if (number < Million)
+        {
+            text.Append(number / Thousand);
+            return CapLength(text).Append("k").ToString();
+        }
+        else
+        {
+            text.Append(number / Million);
+            return CapLength(text).Append("m").ToString();
+        }
+    }
+
+    private static StringBuilder CapLength(StringBuilder stringBuilder)
+    {
+        cappedText.Clear();
+
+        int maxLoops = maxLength;
+
+        for (int i = 0; i < stringBuilder.Length; i++)
+        {
+            if (i >= maxLoops) break;
+
+            char character = stringBuilder[i];
+
+            cappedText.Append(character);
+
+            if (character == ',' || character == '.')
+            {
+                maxLoops += 1;
+            }
+        }
+
+        return cappedText;
     }
 }
