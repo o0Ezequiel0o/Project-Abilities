823fcc929afdf504168895947ab39ba08615e6e3
o0Ezequiel0o <ezematteazzi@gmail.com>
2026-05-17 18:41:30 -0300

    area effect wip

---
+++ Assets/Scripts/AreaEffect/_Base/AreaEffect.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Zeke.PoolableGameObjects;
+
+public abstract class AreaEffect : MonoBehaviour, IPoolableGameObjectConfirmator, IPoolableGameObjectListener
+{
+    [SerializeField] private LayerMask hitLayers;
+
+    [field: Header("Events")]
+    [field: SerializeField] public UnityEvent<AreaEffect> OnDespawn { get; private set; }
+
+    public Action<IPoolableGameObjectConfirmator> PoolableReady { get; set; }
+    public Action<IPoolableGameObjectConfirmator> PoolableBusy { get; set; }
+
+    protected float armorPenetration = 0f;
+    protected float procCoefficient = 0f;
+
+    protected float tickInterval = 0f;
+    protected int currentTick = 0;
+    protected int ticks = 0;
+
+    protected float radius = 0f;
+
+    private float timer = 0f;
+
+    private readonly List<Collider2D> hits = new List<Collider2D>();
+
+    public virtual void OnSentToPool() { }
+
+    public virtual void OnRetrievedFromPool() { }
+
+    //DamageData damageData, GameObject source, Teams team
+    public void CreateAreaEffect(int ticks, float tickInterval, float radius)
+    {
+        this.ticks = ticks;
+        this.tickInterval = tickInterval;
+
+        this.radius = radius;
+    }
+
+    protected abstract void OnTick(List<Collider2D> hits, int count);
+
+    private void Update()
+    {
+        if (ticks > 0)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= tickInterval)
+            {
+                timer = 0f;
+                Tick();
+            }
+        }
+        else
+        {
+            Despawn();
+        }
+    }
+
+    private void Tick()
+    {
+        currentTick += 1;
+
+        ContactFilter2D contactFilter = new ContactFilter2D()
+        {
+            layerMask = hitLayers,
+            useLayerMask = true
+        };
+
+        OnTick(hits, Physics2D.OverlapCircle(transform.position, radius, contactFilter, hits));
+
+        if (currentTick > ticks)
+        {
+            Despawn();
+        }
+    }
+
+    private void Despawn()
+    {
+        OnDespawn?.Invoke(this);
+        PoolableReady?.Invoke(this);
+    }
+}
