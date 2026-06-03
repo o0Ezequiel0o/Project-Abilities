1ecdaf1a96394be9080f7093429d3f56453ca688
o0Ezequiel0o <ezematteazzi@gmail.com>
2026-06-03 17:19:01 -0300

    EntityMove now default move script

--- Assets/Scripts/Entity/Movement/EntityMove.cs
+++ Assets/Scripts/Entity/Movement/EntityMove.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 
-public abstract class EntityMove : MonoBehaviour, IUpgradable
+public class EntityMove : MonoBehaviour, IUpgradable
 {
     [Header("Dependency")]
     [SerializeField] protected EntityPhysics physics;
 
     [Header("Settings")]
     [field: SerializeField] public Stat MoveSpeed { get; private set; }
+    [SerializeField] private float accelerationRatio = 3f;
+    [SerializeField] private float brakingRatio = 2f;
 
-    public abstract Vector2 MoveDirection { get; }
+    public Vector2 MoveDirection => moveDirection;
     public Vector2 Velocity => physics.Velocity;
 
     private Vector2 desiredMoveDirection = Vector2.zero;
+    private Vector2 moveDirection = Vector2.zero;
 
     public void MoveTowards(Vector2 desiredDirection)
     {
@@ -25,7 +28,8 @@ public abstract class EntityMove : MonoBehaviour, IUpgradable
 
     public virtual void FixedUpdate()
     {
-        UpdateMovementInternal(desiredMoveDirection);
+        physics.AddMoveForce(GetForceForDesiredMoveSpeed(MoveSpeed.Value * desiredMoveDirection));
+        moveDirection = desiredMoveDirection;
     }
 
     public virtual void Upgrade()
@@ -38,5 +42,42 @@ public abstract class EntityMove : MonoBehaviour, IUpgradable
         physics = GetComponentInChildren<EntityPhysics>();
     }
 
-    protected abstract void UpdateMovementInternal(Vector2 desiredMoveDirection);
+    private Vector2 GetForceForDesiredMoveSpeed(Vector2 desiredMoveSpeed)
+    {
+        Vector2 forceToReachDesiredSpeed = Vector2.zero;
+
+        forceToReachDesiredSpeed.x = GetForceForDesiredMoveSpeed(desiredMoveSpeed.x, physics.MoveForces.x);
+        forceToReachDesiredSpeed.y = GetForceForDesiredMoveSpeed(desiredMoveSpeed.y, physics.MoveForces.y);
+
+        return forceToReachDesiredSpeed;
+    }
+
+    private float GetForceForDesiredMoveSpeed(float desiredForcesAxis, float currentForcesAxis)
+    {
+        if (desiredForcesAxis != 0f)
+        {
+            float maxForceRequired = Mathf.Sign(desiredForcesAxis) == 1
+                ? Mathf.Max(0, desiredForcesAxis - currentForcesAxis)
+                : Mathf.Min(0, desiredForcesAxis - currentForcesAxis);
+
+            float accelerationStep = MoveSpeed.Value * accelerationRatio * Time.deltaTime;
+            return MoveTowards(maxForceRequired, accelerationStep);
+        }
+        else
+        {
+            float brakingStep = MoveSpeed.Value * brakingRatio * Time.deltaTime;
+
+            float maxForceRequired = -currentForcesAxis;
+            return MoveTowards(maxForceRequired, brakingStep);
+        }
+    }
+
+    private float MoveTowards(float target, float step)
+    {
+        float stepRequired = Mathf.Sign(target) == 1
+            ? Mathf.Min(target, step)
+            : Mathf.Max(target, -step);
+
+        return stepRequired;
+    }
 }
